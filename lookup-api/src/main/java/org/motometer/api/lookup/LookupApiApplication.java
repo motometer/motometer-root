1ec4e57ed0b40c42bae4329e4f3ae3c30dc46d7b
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-06-14 21:42:34 +0300

    Initial commit.

---
+++ lookup-api/src/main/java/org/motometer/api/lookup/LookupApiApplication.java
@@ -0,0 +1,13 @@
+package org.motometer.api.lookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookupApiApplication.class, args);
+	}
+
+}
---
+++ lookup-api/src/test/java/org/motometer/api/lookup/LookupApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.motometer.api.lookup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LookupApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
