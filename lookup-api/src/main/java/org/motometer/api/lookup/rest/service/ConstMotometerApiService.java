55f26cc9e44511df48e9ed4e050956ed22f5c7ec
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-06-15 12:00:46 +0300

    Added dockerfile.

--- lookup-api/src/main/java/org/motometer/api/lookup/rest/service/ConstMotometerApiService.java
+++ lookup-api/src/main/java/org/motometer/api/lookup/rest/service/ConstMotometerApiService.java
@@ -2,12 +2,15 @@ package org.motometer.api.lookup.rest.service;
 
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.Multimap;
+import lombok.extern.slf4j.Slf4j;
 import org.motometer.api.lookup.rest.api.ImmutableMotometerApi;
 import org.motometer.api.lookup.rest.api.MotometerApi;
 import org.motometer.api.lookup.rest.api.MotometerApiVersion;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 public class ConstMotometerApiService implements MotometerApiService {
 
@@ -15,6 +18,9 @@ public class ConstMotometerApiService implements MotometerApiService {
 
     @Override
     public Flux<MotometerApi> services(String env) {
-        return Flux.fromIterable(envMap.get(env));
+        return Mono.just(env)
+                .doOnNext(arg -> log.info("Looking for api for env {}", arg))
+                .map(envMap::get)
+                .flatMapMany(Flux::fromIterable);
     }
 }
